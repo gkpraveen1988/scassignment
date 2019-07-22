89761cf998b557c9bed180d6f06c2e7475a7f3db
Praveen gunasekar <praveen.kumar.gunasekar@autodesk.com>
2019-07-22 08:14:00 +0000

    Updating files

--- terradetails/elb_securitygp.tf
+++ terradetails/elb_securitygp.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "elb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
